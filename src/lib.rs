0de4b9183f044c870ef84ba6e589cc6ddecde2d4
DrBrad <brad@bradeagle.com>
2024-07-09 13:41:23 -0600

    Socket testing

--- src/lib.rs
+++ src/lib.rs
@@ -14,15 +14,21 @@ mod tests {
     fn test() {
         thread::spawn(|| {
             let mut utp_socket = UtpSocket::bind(SocketAddr::from((Ipv4Addr::UNSPECIFIED, 7070))).expect("Failed to bind.");
-            let (packet, src) = utp_socket.receive();
 
-            println!("{} {}", src.to_string(), String::from_utf8_lossy(packet.payload.as_slice()));
+            let (packet, src) = utp_socket.receive();
+            println!("{} {} {}", packet.header.connection_id, src.to_string(), String::from_utf8_lossy(packet.payload.as_slice()));
+
+            utp_socket.send_with_retransmission(&src, "Hello World".as_bytes());
         });
 
         sleep(Duration::from_secs(2));
 
         let mut utp_socket = UtpSocket::bind(SocketAddr::from((Ipv4Addr::UNSPECIFIED, 7072))).expect("Failed to bind.");
+
         utp_socket.send_with_retransmission(&SocketAddr::from((Ipv4Addr::UNSPECIFIED, 7070)), "asdasd".as_bytes());
+
+        let (packet, src) = utp_socket.receive();
+        println!("{} {} {}", packet.header.connection_id, src.to_string(), String::from_utf8_lossy(packet.payload.as_slice()));
 
         loop {
 
