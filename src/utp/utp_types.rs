730257b8026d8cac85c47d09762778887078fd43
DrBrad <brad@bradeagle.com>
2024-07-09 18:47:31 -0600

    Changes to types

--- src/utp/utp_types.rs
+++ src/utp/utp_types.rs
@@ -8,4 +8,23 @@ pub enum UtpTypes {
 
 impl UtpTypes {
 
+    pub fn from_value(value: &u32) -> Result<Self, ()> {
+        for _type in [Self::StData, Self::StFin, Self::StState, Self::StReset, Self::StSyn] {
+            if _type.value().eq(value) {
+                return Ok(_type);
+            }
+        }
+
+        Err(())
+    }
+
+    pub fn value(&self) -> u32 {
+        match self {
+            Self::StData => 0,
+            Self::StFin => 1,
+            Self::StState => 2,
+            Self::StReset => 3,
+            Self::StSyn => 4,
+        }
+    }
 }
