214781e25c7b3a98c9bf713cd86b559b1fbca764
DrBrad <brad@bradeagle.com>
2024-09-12 09:31:48 -0600

    Packet handling

--- src/utp/socket.rs
+++ src/utp/socket.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
-use std::io;
+use std::{io, thread};
 use std::net::{SocketAddr, UdpSocket};
 use std::sync::{Arc, RwLock};
 use crate::utp::stream::UtpStream;
+
+const MAX_UDP_PAYLOAD_SIZE: usize = u16::MAX as usize;
 
 pub struct UtpSocket<P> {
     conns: Arc<RwLock<HashMap<ConnectionId<P>, ConnChannel>>>,
@@ -27,6 +29,24 @@ where
     //where
     //    S: UdpSocket<P> + 'static,
     {
+
+        thread::spawn(move || {
+            let mut buf = [0; MAX_UDP_PAYLOAD_SIZE];
+
+            loop {
+                let (size, src_addr) = {
+                    socket.recv_from(&mut buf).expect("Failed to receive message")
+                };
+
+                let packet;
+
+
+
+
+            }
+        });
+
+
         todo!()
     }
 
