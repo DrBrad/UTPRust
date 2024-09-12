7b37ba91e58513854b6748ee66e62d2fe74e1b5a
DrBrad <brad@bradeagle.com>
2024-09-12 09:14:12 -0600

    Connection changes

--- src/utp/socket.rs
+++ src/utp/socket.rs
@@ -4,18 +4,41 @@ use std::net::{SocketAddr, UdpSocket};
 use std::sync::{Arc, RwLock};
 
 pub struct UtpSocket<P> {
-    //conns: Arc<RwLock<HashMap<ConnectionId<P>, ConnChannel>>>,
+    conns: Arc<RwLock<HashMap<ConnectionId<P>, ConnChannel>>>,
     //accepts: UnboundedSender<Accept<P>>,
     //accepts_with_cid: UnboundedSender<(Accept<P>, ConnectionId<P>)>,
     //socket_events: UnboundedSender<SocketEvent<P>>,
 }
 
 impl UtpSocket<SocketAddr> {
-
     pub fn bind(addr: SocketAddr) -> io::Result<Self> {
         let socket = UdpSocket::bind(addr)?;
-        let socket = Self::with_socket(socket);
-        Ok(socket)
+        Ok(Self::with_socket(socket))
+    }
+}
+
+impl<P> UtpSocket<P>
+where
+    P: ConnectionPeer + 'static,
+{
+
+    pub fn with_socket<S>(mut socket: S) -> Self
+    //where
+    //    S: UdpSocket<P> + 'static,
+    {
+        todo!()
+    }
+
+    pub fn accept(&self)/* -> io::Result<UtpStream>*/ {
+
+    }
+
+    pub fn connect(&self) {
+
+    }
+
+    fn generate_cid(&self) {
+
     }
 }
 
