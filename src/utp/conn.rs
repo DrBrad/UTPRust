0376f0c28c662790fa0e4423cb66d5fe016f3fae
DrBrad <brad@bradeagle.com>
2024-09-13 14:27:14 -0600

    Packet handling

--- src/utp/conn.rs
+++ src/utp/conn.rs
@@ -14,27 +14,27 @@ impl Connection {
 
     pub fn on_packet(&self, packet: UtpPacket) {
         match packet.packet_type() {
-            UtpPacketType::Syn => self.on_syn(),
-            UtpPacketType::State => self.on_state(),
-            UtpPacketType::Data => self.on_data(),
-            UtpPacketType::Fin => self.on_fin(),
+            UtpPacketType::Syn => self.on_syn(packet.seq_num()),
+            UtpPacketType::State => self.on_state(packet.seq_num(), packet.ack_num()),
+            UtpPacketType::Data => self.on_data(packet.seq_num(), packet.payload()),
+            UtpPacketType::Fin => self.on_fin(packet.seq_num(), packet.payload()),
             UtpPacketType::Reset => self.on_reset(),
         }
     }
 
-    fn on_syn(&self) {
+    fn on_syn(&self, seq_num: u16) {
 
     }
 
-    fn on_state(&self) {
+    fn on_state(&self, seq_num: u16, ack_num: u16) {
 
     }
 
-    fn on_data(&self) {
+    fn on_data(&self, seq_num: u16, payload: &Vec<u8>) {
 
     }
 
-    fn on_fin(&self) {
+    fn on_fin(&self, seq_num: u16, payload: &Vec<u8>) {
 
     }
 
